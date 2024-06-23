2d308cf3b2cef53ce8cd7407dfc7aa0f70b2dfa9
krithika1906 <krithika.umasankar@gmail.com>
2024-06-23 21:26:33 +0530

    Intents

---
+++ app/src/main/java/com/example/productitvityplanner2024/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.productitvityplanner2024
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun myClickHandler(view: View) {
+        Log.i("MainActivity","Button Clicked")
+        //var dialIntent = Intent(Intent.ACTION_DIAL)
+        var webIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.yahoo.com"))
+        startActivity(webIntent)
+    }
+}
