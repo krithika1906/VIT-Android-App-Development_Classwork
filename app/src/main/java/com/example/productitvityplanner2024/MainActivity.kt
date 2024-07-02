8501e8b08d3dcaaf7ed5b67589b79f174a84aa6e
krithika1906 <krithika.umasankar@gmail.com>
2024-07-02 11:36:04 +0530

    Activity LifeCycle

--- app/src/main/java/com/example/productitvityplanner2024/MainActivity.kt
+++ app/src/main/java/com/example/productitvityplanner2024/MainActivity.kt
@@ -7,45 +7,58 @@ import android.os.Message
 import android.provider.AlarmClock
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Log.i(TAG, "Activity is created")
+
     }
 
-    fun myClickHandler(view: View) {
-        Log.i("MainActivity","Button Clicked")
-        //var dialIntent = Intent(Intent.ACTION_DIAL)
-        //var webIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.yahoo.com"))
-        //startActivity(webIntent)
-        //createAlarm("milk",22,30)
+    override fun onStart() {
+        super.onStart()
 
-        var hIntent = Intent(this,HomeActivity::class.java)
-        hIntent.putExtra("nkey","krithika-android")
+        Log.e(TAG, "Activity is starting")
+    }
+    override fun onPause() {
+        super.onPause()
+
+        Log.w(TAG, "Activity is Paused")
+    }
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(TAG, "Activity is stopped")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.v(TAG, "activity has got destroyed")
+    }
+
+    fun clickHandler(view) {
+        Log.i("MainActivity- clickhandler", "button clicked")
+        var hIntent = Intent(this, HomeActivity::class.java)
+        hIntent.putExtra("mykey", "android vit krithika")
         startActivity(hIntent)
 
-
-    }
-    fun createAlarm(message: String, hour : Int, minutes :Int){
-        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
-            putExtra(AlarmClock.EXTRA_MESSAGE, message)
-            putExtra(AlarmClock.EXTRA_HOUR, hour)
-            putExtra(AlarmClock.EXTRA_MINUTES,minutes)
+        fun inflateXml() {
+            var nameEditText = EditText(this)
+            nameEditText.setHint("enter your name")
+            var pwdEditText = EditText(this)
+            pwdEditText.setHint("enter your pass")
+            var loginButton = Button(this)
+            loginButton.setText("login")
 
         }
-        //if(intent.resolveActivity(packageManager)!=null){
-            startActivity(intent)
-        //}
     }
 }
